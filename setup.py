a21444593351b2feaf9efd871a31b999a29c10fc
Shouvik <shouvik1999@gmail.com>
2024-04-22 22:55:51 -0600

    requirements add

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'autoParkingManagement',
+    version= '0.0.0',
+    author= 'Shouvik Sengupta',
+    author_email= 'shouvik1999@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
